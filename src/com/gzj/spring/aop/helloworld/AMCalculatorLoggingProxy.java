c546b00895992b55cd1bc8b25350fa7547237663
yuel_global <13373657935_global@163.com>
2020-05-15 14:09:08 +0800

    z

--- src/com/gzj/spring/aop/helloworld/AMCalculatorLoggingProxy.java
+++ src/com/gzj/spring/aop/helloworld/AMCalculatorLoggingProxy.java
@@ -16,10 +16,12 @@ public class AMCalculatorLoggingProxy {
 
 	public AMCalculatior getLoggingProxy() {
 		AMCalculatior proxy=null;
+		//代理对象由哪个类加载器加载
 		ClassLoader loader=target.getClass().getClassLoader();
-		Class[] interfaces = new Class[]{AMCalculatior.class};
-		InvocationHandler h=new InvocationHandler() {
-			
+		//代理对象的类型，即其中有哪些方法
+		Class[] interfaces = {AMCalculatior.class};
+		//当调用代理对象其中的方法时，执行该代码
+		InvocationHandler h=new InvocationHandler() {			
 			@Override
 			public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
 				// TODO Auto-generated method stub
